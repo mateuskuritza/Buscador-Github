e8cf37f63948f4e72109a76c3178296e7a9d721e
mateuskuritza <mateuskuritza@gmail.com>
2021-07-06 19:48:38 -0300

    first configs

---
+++ src/pages/home/Home.js
@@ -0,0 +1,13 @@
+import styled from "styled-components";
+
+export default function Home() {
+    return(
+        <HomeContainer>
+            Olá!
+        </HomeContainer>
+    )
+}
+
+const HomeContainer = styled.div`
+
+`;
